77199d583b808be5d3d8fe7bee990e177ba9cb77
Davkcobraex <120555663+Davkcobraex@users.noreply.github.com>
2023-02-28 14:12:03 +1000

    add fucntion for adding the grids

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
-function defaultGrid(){
-    console.log("apple");
+const newSize = 16;
+const grid = document.getElementById("container");
+
+function setupGrid(size) {
+        grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+        grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+        for (let i = 0; i < size * size; i++) {
+          const gridElement = document.createElement('div')
+          gridElement.classList.add('cells')
+          grid.appendChild(gridElement)
+        }
 }
+
+window.onload = () => {
+    setupGrid(newSize)
+  }
